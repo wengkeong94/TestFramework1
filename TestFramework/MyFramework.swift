8a7410f35759f217de89451b62e012219faaa040
Walter Wong <walter.wwk@grabtaxi.com>
2019-11-19 15:23:18 +0800

    First commit

---
+++ TestFramework/MyFramework.swift
@@ -0,0 +1,14 @@
+//
+//  MyFramework.swift
+//  TestFramework
+//
+//  Created by Walter Wong on 19/11/2019.
+//  Copyright © 2019 Walter Wong. All rights reserved.
+//
+
+import Foundation
+
+public class MyFramework {
+  static let shared = MyFramework()
+  func foo() {}
+}
